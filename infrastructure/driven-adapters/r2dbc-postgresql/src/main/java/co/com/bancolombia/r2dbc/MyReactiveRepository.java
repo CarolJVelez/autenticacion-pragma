50a099d54397eeed323adbd10aa280710c098cdf
Carol Julieth Velez Fernandez <cvelezfe@emeal.nttdata.com>
2025-08-26 13:31:19 -0500

    Avances HU1

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/MyReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/MyReactiveRepository.java
@@ -1,9 +1,14 @@
 package co.com.bancolombia.r2dbc;
 
+import co.com.bancolombia.r2dbc.entities.UserEntity;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 // TODO: This file is just an example, you should delete or modify it
-public interface MyReactiveRepository extends ReactiveCrudRepository<Object, String>, ReactiveQueryByExampleExecutor<Object> {
+public interface MyReactiveRepository extends ReactiveCrudRepository<UserEntity, String>, ReactiveQueryByExampleExecutor<UserEntity> {
 
+    Mono<Void> deleteByUserId(String userId);
+
+    Mono<Boolean> existsByEmail(String email);
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,29 +1,36 @@
 package co.com.bancolombia.api;
 
+import co.com.bancolombia.api.dto.request.CreateUserDTO;
+import co.com.bancolombia.api.mapper.UserDTOMapper;
+import co.com.bancolombia.usecase.user.interfaces.IUserUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 @Component
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
 
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
+    private final IUserUseCase userUseCase;
+    private final RequestValidator validator;
+    private final UserDTOMapper mapper;
 
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
 
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> createUser(ServerRequest req) {
+        return req.bodyToMono(CreateUserDTO.class)
+                .flatMap(validator::validate)
+                .map(mapper::toModel)
+                .flatMap(userUseCase::create)
+                .flatMap(saved -> ServerResponse
+                        .created(URI.create("/api/users/" + saved.getUserId()))
+                        .contentType(APPLICATION_JSON)
+                        .bodyValue(mapper.toDto(saved))
+                );
     }
 }
